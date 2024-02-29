15ae117d25f3134790dc477b7569d8265c8441e1
Dumebi Okolo <okolodumebi@gmail.com>
2024-02-29 22:51:28 +0100

    Add devise

--- app/controllers/about_controllers.rb
+++ app/controllers/about_controllers.rb
@@ -1,6 +1,11 @@
-class AboutControllers < ApplicationController
+class AboutController < ApplicationController
 def index
+  flash.now[:notice] = "Logged in successfully!"
+  flash.now[:alert] = "Invalid email or password"
 end
 def home
+  if session[:user_id]
+    @user = User.find_by(id: session[:user_id])
+end
 end
 end
